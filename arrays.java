24df7d3405731a5eea758984d199d491424190c1
sir-Omoz bl@k <132283101+Sir-omoz@users.noreply.github.com>
2024-05-28 06:08:02 -0700

    Add files via upload

---
+++ arrays.java
@@ -0,0 +1,31 @@
+//arrays-homogenious collection of element  add, sort, search, remove, update
+
+int[] intArray = new int[5]
+
+static int[] anotherArray = {4,6,9,8,7};
+//an example
+
+
+public class ArrayDemo {
+    public static.void.main(string[] args){
+        int[] numbersArray = new int[10];
+        for(int i=0; i<numbersArray.lenght; i++){
+            system.out.printIn(numbersArray[i]);
+        }
+    }
+
+}
+//updating is overiting a value...leterally just changing the value
+int[] intArray = new int[5]
+//trail
+static int[] anotherArray = {4,6,9,8,7};
+system.out.printin(int[4], int[3], int[2],int[1], int[0]);
+//anotherway
+system.out.printin(int[4-0])
+//correct
+for (int i = numbersArrays.lenght-1; i >=0; i--)
+
+
+Strianword = "test";
+cha[] wordArray = wordtoChrArray();
+//find out an string is a falindrome(reverse is equal eith the actual)
---
+++ interferance.java
@@ -0,0 +1,75 @@
+publc_interface Electronic {
+boolean off;
+boolean on;
+}
+publc class Computer implement Electronic{
+    public boolean on() {
+        reeturn false;
+    }
+    @override
+    public boolean off();
+}
+boolean onOrOff= false;
+
+boolean on();
+
+defuat void electronic(){
+    if(onOrOff){
+        system.out.printin("default");
+    }
+}
+//an example
+public class processor {
+
+    string type = corei5;
+    string generation = "8th gen";
+}
+public class RAM{
+    string name = DDR4;
+    string size = 16GB;
+}
+public class storage{
+    sting type = "SSD";
+    string manufacturer = "samsung";
+    int capacity = 512;
+}
+RAM ram;
+
+Processor cpu;
+
+Storage storage;
+public Computer(RAM ram, Processor cpu, Storage storage){
+    this.ram = ram;
+    this.cpu = cpu;
+    this.storage = storage;
+}
+public class Admin extends User {
+
+    static void addNummer(int a, int b){
+        system.out.printin(a+b);
+    }
+    public static void main(string[] args){
+        addNumber(a=4, b=5)
+    }
+    string staffId;
+    void uploadResults(string course, float score){
+        scanner AdminInput = new Scanner(system.in);
+        system.out.printIn(course + scoree); 
+    }
+    public static void main(string[] args){
+        Admin staffId = new Admin();
+        staffId.password = "New staff";
+        system.out.printIn(staffId.name)
+    }
+    Computer {
+        new Computer(new RAM(), new Processor(), new storage())
+    }
+}
+public String toString(){
+    return computer{
+        ram = ram;
+        processor = cpu;
+        storage = storage;
+    }
+    return(ram(), processor(), storage())
+}
